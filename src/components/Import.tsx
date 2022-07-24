9ec063530cd748416022887ee816388463be31ea
NamNguyen <imnamnv@gmail.com>
2022-07-24 15:40:31 +0700

    added feature import like quizlet

--- src/components/Import.tsx
+++ src/components/Import.tsx
@@ -11,6 +11,11 @@ import {
   Box,
   Button as ButtonMUI,
   Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
   TextField,
   Typography,
 } from "@material-ui/core";
@@ -23,9 +28,42 @@ export default () => {
   >(CategoryContext);
 
   const [name, setName] = useState("");
+  const [fileName, setFileName] = useState("");
+
+  const [termAndDefinitionValue, setTermAndDefinitionValue] = useState("");
+  const [customTermAndDefinitionValue, setCustomTermAndDefinitionValue] =
+    useState(" - ");
+
+  const [rowsValue, setRowsValue] = useState("");
+  const [customRowsValue, setCustomRowsValue] = useState("\\n\\n");
+
+  const [resultText, setResultText] = useState<string | ArrayBuffer>("");
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
+  };
+
+  const handleTermAndDefinitionValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setTermAndDefinitionValue(event.target.value);
+  };
+  const handleCustomTermAndDefinitionValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCustomTermAndDefinitionValue(event.target.value);
+  };
+
+  const handleRowsValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsValue(event.target.value);
+  };
+
+  const handleCustomRowsValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCustomRowsValue(event.target.value);
   };
 
   const showFile = async (event: React.ChangeEvent) => {
@@ -35,12 +73,8 @@ export default () => {
     const reader = new FileReader();
     reader.onload = async (e) => {
       const text = e.target.result;
-      const newCategory = convertToCategoryList(name, text);
-      addNewCategory({
-        newCategory: { id: uuidv4(), ...newCategory },
-      });
-
-      setCurrentStatus({ currentStatus: ROOT_STATUS.LEARNING });
+      setResultText(text);
+      setFileName(target.files[0].name);
     };
     reader.readAsText(target.files[0]);
   };
@@ -49,21 +83,31 @@ export default () => {
     setCurrentStatus({ currentStatus: ROOT_STATUS.LEARNING });
   };
 
+  const handleImport = () => {
+    const newCategory = convertToCategoryList(
+      name,
+      resultText,
+      termAndDefinitionValue === "custom"
+        ? customTermAndDefinitionValue
+        : termAndDefinitionValue,
+      rowsValue === "custom" ? customRowsValue : rowsValue
+    );
+
+    addNewCategory({
+      newCategory: { id: uuidv4(), ...newCategory },
+    });
+
+    setCurrentStatus({ currentStatus: ROOT_STATUS.LEARNING });
+  };
+
   return (
     <Box width={"100%"}>
       <Container>
         <Typography color="error" variant="body2">
-          *Note: Format file must be:
+          *Note: Format file(.txt) must be the same format as the file(.txt)
+          which was exported from Quizlet
         </Typography>
-        <Typography color="error" variant="body2">
-          front1|back1
-        </Typography>
-        <Typography color="error" variant="body2">
-          front2|back2
-        </Typography>
-        <Typography color="error" variant="body2">
-          ...
-        </Typography>
+
         <Box my={1}>
           <TextField
             fullWidth
@@ -74,6 +118,81 @@ export default () => {
             variant="outlined"
           />
         </Box>
+        <Box display="flex" alignItems={"center"}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">
+              Between term and definition
+            </FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="betweenTermAndDefinition"
+              value={termAndDefinitionValue}
+              onChange={handleTermAndDefinitionValueChange}
+            >
+              <FormControlLabel value="\t" control={<Radio />} label="Tab" />
+              <FormControlLabel
+                value=","
+                control={<Radio />}
+                label="Comma (,)"
+              />
+
+              <FormControlLabel
+                value={"custom"}
+                control={<Radio />}
+                label={
+                  <TextField
+                    fullWidth
+                    disabled={
+                      termAndDefinitionValue === "custom" ? false : true
+                    }
+                    size="small"
+                    value={customTermAndDefinitionValue}
+                    onChange={handleCustomTermAndDefinitionValueChange}
+                    label={"Custom"}
+                    variant="outlined"
+                  />
+                }
+              />
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Between rows</FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="betweenRow"
+              value={rowsValue}
+              onChange={handleRowsValueChange}
+            >
+              <FormControlLabel
+                value="\n"
+                control={<Radio />}
+                label="New line"
+              />
+              <FormControlLabel
+                value=";"
+                control={<Radio />}
+                label="Semicolon (;)"
+              />
+
+              <FormControlLabel
+                value="custom"
+                control={<Radio />}
+                label={
+                  <TextField
+                    fullWidth
+                    disabled={rowsValue === "custom" ? false : true}
+                    size="small"
+                    value={customRowsValue}
+                    onChange={handleCustomRowsValueChange}
+                    label={"Custom"}
+                    variant="outlined"
+                  />
+                }
+              />
+            </RadioGroup>
+          </FormControl>
+        </Box>
 
         <Box
           textAlign={"right"}
@@ -81,15 +200,23 @@ export default () => {
           alignItems={"center"}
           justifyContent={"flex-end"}
         >
-          <ButtonMUI
-            component="label"
-            variant="contained"
-            startIcon={<ImportExportIcon />}
-            color={"primary"}
-          >
-            Upload Txt
-            <input type="file" accept=".txt" hidden onChange={showFile} />
-          </ButtonMUI>
+          {resultText ? (
+            <Button
+              title={`Import with ${fileName}`}
+              handleOnClick={handleImport}
+              color={"primary"}
+            />
+          ) : (
+            <ButtonMUI
+              component="label"
+              variant="contained"
+              startIcon={<ImportExportIcon />}
+              color={"primary"}
+            >
+              Upload Txt
+              <input type="file" accept=".txt" hidden onChange={showFile} />
+            </ButtonMUI>
+          )}
           <Box ml={1}>
             <Button
               title="Cancel"
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,28 +1,41 @@
 import { Category } from "../models";
 import { v4 as uuidv4 } from "uuid";
+
 export function shuffleAray(array) {
   return array.sort(() => Math.random() - 0.5);
 }
 
 export function convertToCategoryList(
   name: string,
-  data: string | ArrayBuffer
+  data: string | ArrayBuffer,
+  termAndDefinitionValue: string,
+  rowsValue: string
 ) {
   const result: Category = {
     name: name || "New Category",
     data: [],
   };
   if (typeof data === "string") {
-    const newArray = data.split(/\r\n/);
+    const regexTermAndDefinition = new RegExp(
+      `${termAndDefinitionValue.replace(/\\n?\n?/g, "\r\n")}`
+    );
+
+    const regexRows = new RegExp(`${rowsValue.replace(/\\n?\n?/g, "\r\n")}`);
+
+    const newArray = data.split(regexRows);
+
     for (let i = 0; i < newArray.length; i++) {
       const element = newArray[i];
-      const [front, back] = element.split("|");
-      result.data.push({
-        id: uuidv4(),
-        back: back || "Wrong format file",
-        front: front || "Wrong format file",
-      });
+      if (element) {
+        const [front, back] = element.split(regexTermAndDefinition);
+        result.data.push({
+          id: uuidv4(),
+          back: back || "Wrong format file",
+          front: front || "Wrong format file",
+        });
+      }
     }
   }
+
   return result;
 }
